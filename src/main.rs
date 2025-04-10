e4ed4d1a64c473c9a6cd394d680b2ea6f7f4fd10
JT Raber <JTRaber@discount-drugmart.com>
2025-04-10 14:23:53 -0400

    theme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use crossterm::event::{self, Event, KeyCode, KeyEventKind, KeyModifiers};
 use ratatui::{
     buffer::Buffer,
@@ -13,9 +14,29 @@ use tui_widgets::big_text::BigText;
 
 mod tui;
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    fg: Option<Color>,
+
+    #[arg(short, long)]
+    bg: Option<Color>,
+}
+
+#[derive(Clone, Copy)]
 pub struct Theme {
     fg: Color,
     bg: Color,
+}
+
+impl Default for Theme {
+    fn default() -> Self {
+        Theme {
+            fg: Color::White,
+            bg: Color::Black,
+        }
+    }
 }
 
 pub struct TimersState {
@@ -27,9 +48,15 @@ pub struct Timers {
     exit: bool,
     running: bool,
     timer: Duration,
+    theme: Theme,
 }
 
 impl Timers {
+    pub fn with_theme(mut self, theme: Theme) -> Self {
+        self.theme = theme;
+        self
+    }
+
     pub fn run(&mut self, terminal: &mut Tui) -> Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.render_frame(frame))?;
@@ -40,10 +67,7 @@ impl Timers {
 
     fn render_frame(&mut self, frame: &mut Frame) {
         let state = &mut TimersState {
-            theme: Theme {
-                fg: Color::Yellow,
-                bg: Color::Red,
-            },
+            theme: self.theme
         };
 
         frame.render_stateful_widget(self, frame.area(), state);
@@ -124,8 +148,17 @@ impl StatefulWidget for &mut Timers {
 }
 
 fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    let default_theme = Theme::default();
+
+    let theme = Theme {
+        fg: cli.fg.unwrap_or(default_theme.fg),
+        bg: cli.bg.unwrap_or(default_theme.bg),
+    };
+
     let mut terminal = tui::init()?;
-    let mut timers = Timers::default();
+    let mut timers = Timers::default().with_theme(theme);
     let timers_result = timers.run(&mut terminal);
     tui::restore()?;
     timers_result
