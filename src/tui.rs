c44d7dc4a5da3fa20a0c970d7a35dfcdee6e4b44
JT Raber <JTRaber@discount-drugmart.com>
2025-04-10 14:09:49 -0400

    i <3 clippy and formatting

--- src/tui.rs
+++ src/tui.rs
@@ -8,16 +8,16 @@ pub type Tui = Terminal<CrosstermBackend<Stdout>>;
 
 /// Initialize the terminal
 pub fn init() -> io::Result<Tui> {
-	execute!(stdout(), EnterAlternateScreen)?;
-	enable_raw_mode()?;
-	let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
-	terminal.clear()?;
-	return Ok(terminal);
+    execute!(stdout(), EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+    Ok(terminal)
 }
 
 /// Restore the terminal to its original state
 pub fn restore() -> io::Result<()> {
-	execute!(stdout(), LeaveAlternateScreen)?;
-	disable_raw_mode()?;
-	Ok(())
+    execute!(stdout(), LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
